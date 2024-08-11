3f29d8420ce59c989534f8a5d7b1b3001abfe1b3
Harsh Jain <harrshjain25@gmail.com>
2024-08-12 00:01:40 +0530

    small updates

--- index.php
+++ index.php
@@ -1,11 +1,8 @@
 <!doctype html>
 <html lang="en">
   <head>
-    <!-- Required meta tags -->
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-
-    <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
     <link rel="stylesheet" type="text/css" href="css/style.css">
     <link rel="stylesheet" type="text/css" href="css/navbar.css">
@@ -19,7 +16,6 @@
   ?>
   
       <div class="container-fluid">
-      <!-- Introduction section -->
             <div class="row intro py-1">
               <div class="col-sm-12 col-md">
                 <div class="heading text-center my-5">
@@ -31,8 +27,6 @@
                 <img src="img/bank.png" class="img-fluid pt-2">
               </div>
             </div>
-
-      <!-- Activity section -->
             <div class="row activity text-center">
                   <div class="col-md act">
                     <img src="img/user.jpg" class="img-fluid">
@@ -52,7 +46,7 @@
             </div>
       </div>
       <footer class="text-center mt-5 py-2">
-        <p>&copy 2020. Made by <b>SHIVANGI AGARWAL</b> <br> The Sparks Foundation</p>
+        <p>&copy 2024. Made by <b>Harsh Deepak jain</b> <br> The Sparks Foundation</p>
       </footer>
       <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ho+j7jyWK8fNQe+A12Hb8AhRq26LrZ/JpcUGGOn+Y7RsweNrtN/tE3MoK7ZeZDyx" crossorigin="anonymous"></script>
